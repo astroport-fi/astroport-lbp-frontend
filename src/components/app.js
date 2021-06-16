b8c253504d121b5c72d34f6e761c79b0e5fdc8f5
Jim Ryan <jim@room118solutions.com>
2021-06-16 15:20:58 -0400

    Remove header

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,9 @@
 import './app.css';
-import Header from './header.js';
 import TokenSales from './token_sales.js';
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <Header />
+    <div className="container mx-auto mt-10">
       <TokenSales />
     </div>
   );
