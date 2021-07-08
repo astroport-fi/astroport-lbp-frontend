c082fd25a237266327bb7f8a3a8809e2ba9c08d6
Jim Ryan <jim@room118solutions.com>
2021-07-08 19:25:55 -0400

    Move SVGs to files

--- src/components/swap_form.js
+++ src/components/swap_form.js
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
 import AssetInput from './asset_input';
+import { ReactComponent as DownArrow } from '../assets/images/down-arrow.svg';
+import { ReactComponent as ReverseArrows } from '../assets/images/reverse-arrows.svg';
 
 function SwapForm({
   onSubmit,
@@ -46,20 +48,13 @@ function SwapForm({
 
       <div className="relative flex items-center justify-center">
         <div className="rounded-full bg-blue-gray-500 text-yellow w-8 h-8 p-1 flex items-center justify-center my-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd"
-                  d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
-                  clipRule="evenodd"/>
-          </svg>
+          <DownArrow className="h-5 w-5" />
         </div>
 
         <button type="button" className="absolute right-8" onClick={onReverseAssets}>
           <span className="sr-only">Reverse assets</span>
 
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-60" viewBox="0 0 20 20" fill="currentColor">
-            <path
-              d="M5 12a1 1 0 102 0V6.414l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L5 6.414V12zM15 8a1 1 0 10-2 0v5.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L15 13.586V8z"/>
-          </svg>
+          <ReverseArrows className="h-5 w-5 opacity-60" />
         </button>
       </div>
 
