031074ae9d8ca65f7aa787e7051bf1e5ae11b3bf
Jim Ryan <jim@room118solutions.com>
2021-07-08 19:04:46 -0400

    Add down arrow between swap form fields

--- src/components/swap_form.js
+++ src/components/swap_form.js
@@ -43,12 +43,19 @@ function SwapForm({
         step={fromStep}
       />
 
+      <div className="rounded-full bg-blue-gray-500 text-yellow w-8 h-8 p-1 inline-block flex items-center justify-center mx-auto my-4">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd"
+                d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"/>
+        </svg>
+      </div>
+
       <AssetInput
         label="To (estimated)"
         amount={toAmount}
         onAmountChange={toAmountChange}
         usdAmount={toUSDAmount}
-        className="mt-10"
         assets={assets}
         selectedAsset={toAsset}
         onAssetChange={toAssetChange}
