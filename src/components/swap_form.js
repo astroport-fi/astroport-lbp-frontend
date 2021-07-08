b7a17e241f5892332509a187e81106cfc91b882d
Jim Ryan <jim@room118solutions.com>
2021-07-08 19:19:25 -0400

    Fix SVG path attribute names

--- src/components/swap_form.js
+++ src/components/swap_form.js
@@ -47,9 +47,9 @@ function SwapForm({
       <div className="relative flex items-center justify-center">
         <div className="rounded-full bg-blue-gray-500 text-yellow w-8 h-8 p-1 flex items-center justify-center my-4">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd"
+            <path fillRule="evenodd"
                   d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"/>
+                  clipRule="evenodd"/>
           </svg>
         </div>
 
