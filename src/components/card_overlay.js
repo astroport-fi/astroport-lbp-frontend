14a82ba13352ef2f44221e52905cc946c5a2ffd4
Jim Ryan <jim@room118solutions.com>
2021-07-09 12:39:58 -0400

    Use inset-px class instead of doing it manually and also stop covering border

--- src/components/card_overlay.js
+++ src/components/card_overlay.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 
 function CardOverlay({ className, children }) {
   return (
-    <div className={classNames('absolute top-0 left-0 w-full h-full rounded-xl bg-opacity-80 z-50 flex justify-center items-center', className)}>
+    <div className={classNames('absolute inset-px rounded-xl bg-opacity-80 z-50 flex justify-center items-center', className)}>
       <div className="text-center">
         {children}
       </div>
