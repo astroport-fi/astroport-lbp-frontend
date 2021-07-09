30c52c01ee296a1acc884a3510aeaacb36cceedc
Jim Ryan <jim@room118solutions.com>
2021-07-09 19:47:52 -0400

    Fade weights chart popover in

--- src/components/info_card/popover.js
+++ src/components/info_card/popover.js
@@ -1,13 +1,21 @@
+import 'animate.css/source/_vars.css';
+import 'animate.css/source/_base.css';
+import 'animate.css/source/fading_entrances/fadeIn.css';
+import classNames from 'classnames';
+
 function Popover({ children }) {
+  const commonClasses = ['z-50', 'animated', 'fadeIn', 'faster', 'cursor-default'];
+  const bgOpacityClass = 'bg-opacity-80';
+
   return (
     <>
       {/* Popover triangle */}
-      <div className="inset-x-0 overflow-hidden absolute flex justify-center opacity-80 cursor-default z-50">
-        <div className="h-5 w-5 bg-blue-gray-700 rotate-45 transform origin-bottom-left"></div>
+      <div className={classNames('inset-x-0 overflow-hidden absolute flex justify-center opacity-80', commonClasses)}>
+        <div className={classNames('h-5 w-5 bg-blue-gray-700 rotate-45 transform origin-bottom-left', bgOpacityClass)}></div>
       </div>
 
       {/* Popover wrapper, right-aligned (only current needed use-case) */}
-      <div className="absolute bg-blue-gray-700 bg-opacity-80 mt-4 p-2 rounded-lg shadow min-w-full right-0 cursor-default z-50">
+      <div className={classNames('absolute bg-blue-gray-700 bg-opacity-80 mt-4 p-2 rounded-lg shadow min-w-full right-0', bgOpacityClass, commonClasses)}>
         {children}
       </div>
     </>
