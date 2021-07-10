6d7245cd5dc04171c3a1cac71b2f9e83c2d1a451
Jim Ryan <jim@room118solutions.com>
2021-07-09 21:58:48 -0400

    Change background color of weight chart popover to give it more of a frosted glass look

--- src/components/info_card/popover.js
+++ src/components/info_card/popover.js
@@ -5,17 +5,18 @@ import classNames from 'classnames';
 
 function Popover({ children }) {
   const commonClasses = ['z-50', 'animated', 'fadeIn', 'faster', 'cursor-default', 'backdrop-filter', 'backdrop-blur'];
-  const bgOpacityClass = 'bg-opacity-70';
+  const bgOpacityClass = 'bg-opacity-20';
+  const bgColorClass = 'bg-black';
 
   return (
     <>
       {/* Popover triangle */}
       <div className={classNames('inset-x-0 overflow-hidden absolute flex justify-center opacity-80', commonClasses)}>
-        <div className={classNames('h-5 w-5 bg-blue-gray-700 rotate-45 transform origin-bottom-left', bgOpacityClass)}></div>
+        <div className={classNames('h-5 w-5 rotate-45 transform origin-bottom-left', bgOpacityClass, bgColorClass)}></div>
       </div>
 
       {/* Popover wrapper, right-aligned (only current needed use-case) */}
-      <div className={classNames('absolute bg-blue-gray-700 mt-4 p-2 rounded-lg shadow-xl min-w-full right-0', bgOpacityClass, commonClasses)}>
+      <div className={classNames('absolute mt-4 p-2 rounded-lg shadow-xl min-w-full right-0', bgOpacityClass, bgColorClass, commonClasses)}>
         {children}
       </div>
     </>
