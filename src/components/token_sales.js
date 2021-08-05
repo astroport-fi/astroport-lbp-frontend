081b363342c7d48ee240d3fec4ef4bd0c0e43f60
Jim Ryan <jim@room118solutions.com>
2021-08-05 17:25:54 -0400

    Clear current pair when there is no current pair
    This can happen when the current pair is set on the default network,
    but then the extension is connected and is connected to a different network,
    and there is no current pair/sale on that network.

--- src/components/token_sales.js
+++ src/components/token_sales.js
@@ -63,6 +63,8 @@ function TokenSales() {
           setSaleTokenInfo(
             await getTokenInfo(terraClient, saleTokenAddress)
           );
+        } else {
+          setCurrentPair();
         }
       } catch(e) {
         reportException(e);
