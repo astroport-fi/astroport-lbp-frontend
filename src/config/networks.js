0b427ac957af7d5d01d4f62db9fdffb2b37cf548
Jim Ryan <jim@room118solutions.com>
2021-11-26 15:51:17 -0500

    Add UST/TESTA demo testnet pair

--- src/config/networks.js
+++ src/config/networks.js
@@ -18,7 +18,7 @@ export const networks = {
     factoryContractAddress: 'terra1nt459erudaug8d2vsqjmhv7zc9c9vmamj3esn9',
     gasPrices: { uusd: 0.15 },
     allowedPairContracts: [
-
+      'terra1vmsnmx6q3amlhrkzy8yelefxdv572rw5m4qeec' // TESTA
     ]
   },
   ...envNetworks
